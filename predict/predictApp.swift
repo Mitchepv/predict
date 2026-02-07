730d0a4d6c1e98e349071a7ed640770ca54e02f6
Mitchepv <143571339+Mitchepv@users.noreply.github.com>
2026-02-07 14:14:39 -0500

    Initial Commit

---
+++ predict/predictApp.swift
@@ -0,0 +1,17 @@
+//
+//  predictApp.swift
+//  predict
+//
+//  Created by Nia Mitchell on 2/7/26.
+//
+
+import SwiftUI
+
+@main
+struct predictApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
